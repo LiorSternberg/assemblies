3a19a88536e9315f479d76f32efff29afc3e4207
Nimrod Pansky <nimrod@optibus.com>
2020-03-30 21:10:52 +0300

    wip

--- For Reference/Learning/question_2.py
+++ For Reference/Learning/question_2.py
@@ -1,9 +1,21 @@
 from brain import Brain
 
-P = 0.1
+P = 0.01
+BETA = 0.05
+N = 10**7
+K = 10**4
 
 
 def get_neuron_status():
-    """
-    """
-    Brain(P)
+    brain = Brain(P)
+    brain.add_area(name='A', n=10**7, k=K, beta=BETA)
+    brain.add_area(name='B', n=10**7, k=K, beta=BETA)
+    brain.add_area(name='C', n=10**7, k=K, beta=BETA)
+
+    output_area = brain.areas['A']
+    stimulus_name = 'Stimulus'
+    brain.add_stimulus(name=stimulus_name, k=K)
+    brain.project({stimulus_name: ['A']}, {})
+
+
+get_neuron_status()
