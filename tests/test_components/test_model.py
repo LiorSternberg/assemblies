d5e244565734d9c9e4462ff84bfb8bdc9b9c8612
Lior Sternberg <liors1@mail.tau.ac.il>
2020-07-13 00:38:00 +0300

    Removed obsolete simulations written as unit tests (sanity tests).

--- tests/test_components/test_model.py
+++ tests/test_components/test_model.py
@@ -7,7 +7,7 @@ from tests.test_components.learning_component_test_base import LearningComponent
 
 class TestLearningModel(LearningComponentTestBase):
 
-    def test_run_model_sanity(self):
+    def test_run_model_input_allowed_range(self):
         model = LearningModel(brain=self.brain, sequence=self.sequence, input_stimuli=self.input_stimuli)
         self.assertIn(model.run_model(0), [0, 1])
         self.assertIn(model.run_model(1), [0, 1])
@@ -27,13 +27,3 @@ class TestLearningModel(LearningComponentTestBase):
         self.assertEqual(result_11, result_11_2)
         self.assertEqual(result_00, result_00_2)
 
-    def test_train_model_sanity(self):
-        model = LearningModel(brain=self.brain, sequence=self.sequence, input_stimuli=self.input_stimuli)
-
-        training_set = create_data_set_from_list([0, 1, 0, 1])
-
-        model.train_model(training_set, number_of_sequence_cycles=50)
-        test_results = model.test_model(training_set)
-        self.assertEqual(1, test_results.accuracy)
-        self.assertEqual([], test_results.false_negative)
-        self.assertEqual([0, 1, 2, 3], test_results.true_positive)
